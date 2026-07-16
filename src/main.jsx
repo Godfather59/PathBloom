7ebeea0ceeffa72777a1082e8833bc77fd27afba
Godfather <said59987@gmail.com>
2026-07-16 13:18:17 +0100

    Load redesigned PathBloom visual system

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import './components/NewFeatures.css';
 import './components/MobilePolish.css';
 import './components/ArabicRTL.css';
+import './components/DesignSystem.css';
 
 const rootElement = document.getElementById('root');
 

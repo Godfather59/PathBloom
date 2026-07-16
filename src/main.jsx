96545ee6a40b27d766fa026e96941e6a9492e6fe
Godfather <said59987@gmail.com>
2026-07-16 14:09:53 +0100

    Load migrated feature styles

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './components/DecisionSheet.css';
 import './components/DestinationScreens.css';
 import './components/SystemMenuRedesign.css';
 import './components/PhaseTwoScreens.css';
+import './components/PhaseTwoFeatureStyles.css';
 
 const rootElement = document.getElementById('root');
 
